d0fbcd3f9b88056ac666b0819a885949dba56959
unknown <Mdshowanhossain11@gmail.com>
2021-08-28 23:36:24 +0600

    NODE-EXPRESS-MONGOOSE-BLOG-APP

--- database/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/Blog-Application')
-    .then(() => console.log('Your Database Connection Successfully!'))
-    .catch((err) => console.log(err))
